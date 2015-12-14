8c24781d7cccf30d8bfcf495fe3f1d639c74042b
Tony Holdstock-Brown <tony@docker.com>
2015-12-14 13:11:05 -0800

    Assert using specified ui key works as expected

--- test/ui/key.js
+++ test/ui/key.js
@@ -15,10 +15,13 @@ let store = createStore(combineReducers({ ui: reducer }));
 
 describe('key generation', () => {
 
-  class TestWithoutKey extends Component {
+  class Test extends Component {
     render() { return <p>Hi</p>; }
   }
-  const WrappedTestWithoutKey = ui({})(TestWithoutKey);
+  const testKey = 'testKey'
+
+  const WrappedTestWithoutKey = ui({})(Test);
+  const WrappedTestWithKey = ui({key: testKey})(Test);
 
   describe('opts.key === undefined', () => {
     it('assigns a random key to the component', () => {
@@ -32,18 +35,34 @@ describe('key generation', () => {
         </Provider>
       );
       const comps = TestUtils
-        .scryRenderedComponentsWithType(tree, TestWithoutKey);
+        .scryRenderedComponentsWithType(tree, Test);
       const { uiKey } = comps[0].props;
 
       // Check basic setup of the UI key within the first component
       assert(uiKey !== '', 'uiKey is not empty');
-      assert(uiKey.substr(0, 'TestWithoutKey'.length) === 'TestWithoutKey', 'Key begins with component name');
-      assert(uiKey.length >= ('TestWithoutKey'.length + 5));
+      assert(uiKey.substr(0, 'Test'.length) === 'Test', 'Key begins with component name');
+      assert(uiKey.length >= ('Test'.length + 5));
 
       // Ensure that all three components have unique IDs by creating a set
       // of each key and checking the set's length
       const uniqs = Array.from(new Set(comps.map(c => c.props.uiKey)));
       assert(uniqs.length === 3, 'Two unique keys are specified');
+    });
+  });
+
+  describe('opts.key !== undefined', () => {
+    it('uses the specified key', () => {
+      const tree = TestUtils.renderIntoDocument(
+        <Provider store={ store }>
+          <WrappedTestWithKey />
+        </Provider>
+      );
+
+      const c = TestUtils.findRenderedComponentWithType(tree, Test);
+      const { uiKey } = c.props;
+
+      // Check basic setup of the UI key within the first component
+      assert(uiKey === testKey, 'uiKey matches opts.key');
     });
   });
 
