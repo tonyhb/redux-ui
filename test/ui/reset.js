609004b415d31ca2dfbf3e935bcea7645ebf0a70
Kei Takashima <Kei.Takashima@crssoftware.co.nz>
2016-02-14 12:36:39 +1300

    update defaults, reducer, reset and context tests to use both redux and redux-immutablejs combine reducers

--- test/ui/reset.js
+++ test/ui/reset.js
@@ -1,61 +1,65 @@
 'use strict';
 
-import { assert } from 'chai'; 
+import { assert } from 'chai';
 import React, { Component } from 'react';
 import TestUtils from 'react-addons-test-utils';
 import shallowEqual from 'react-redux/lib/utils/shallowEqual';
 
 import ui, { reducer } from '../../src';
-import { render, renderAndFind } from '../utils/render.js';
+import { render, renderAndFind, setCombineReducers } from '../utils/render.js';
 
-describe('resetting UI state', () => {
-  class Parent extends Component {
-    render = () => (<div>{ this.props.children }</div>)
-  }
+['reduxCombineReducers', 'immutableCombineReducers'].forEach((cr) => {
+  describe('resetting UI state', () => {
+    before(() => setCombineReducers(cr));
 
-  class Child extends Component {
-    render = () => <p>Child</p>
-  }
+    class Parent extends Component {
+      render = () => (<div>{ this.props.children }</div>)
+    }
 
-  const UIParent = ui({ state: { name: 'parent' } })(Parent);
-  const UIChild = ui({ state: { name: 'child' } })(Child);
+    class Child extends Component {
+      render = () => <p>Child</p>
+    }
 
-  it('resetting a UI component with no children resets its own UI state', () => {
-    const child = renderAndFind(<UIChild />, Child);
+    const UIParent = ui({ state: { name: 'parent' } })(Parent);
+    const UIChild = ui({ state: { name: 'child' } })(Child);
 
-    child.props.updateUI({ name: 'foobar' });
-    assert(child.props.ui.name === 'foobar');
-    child.props.resetUI();
-    assert(child.props.ui.name === 'child');
-  });
+    it('resetting a UI component with no children resets its own UI state', () => {
+      const child = renderAndFind(<UIChild />, Child);
 
-  it('resetting a parent UI resets its own AND child contexts', () => {
-      const tree = render(<UIParent><UIChild /></UIParent>);
-      const parent = TestUtils.findRenderedComponentWithType(tree, Parent);
-      const child = TestUtils.findRenderedComponentWithType(tree, Child);
-
-      child.props.updateUI({ name: 'b' });
-      parent.props.updateUI({ name: 'a' });
-      // Ensure state is updated
-      assert(child.props.ui.name === 'b');
-      // Resetting a parent should also reset all children
-      parent.props.resetUI();
-      assert(child.props.ui.name === 'child');
-      assert(parent.props.ui.name === 'parent');
-  });
-
-  it('resetting a child UI resets only its own context', () => {
-      const tree = render(<UIParent><UIChild /></UIParent>);
-      const parent = TestUtils.findRenderedComponentWithType(tree, Parent);
-      const child = TestUtils.findRenderedComponentWithType(tree, Child);
-
-      child.props.updateUI({ name: 'b' });
-      parent.props.updateUI({ name: 'a' });
-      // Ensure state is updated
-      assert(child.props.ui.name === 'b');
-      // Child should only reset itself to child; parent should stay changed
+      child.props.updateUI({ name: 'foobar' });
+      assert(child.props.ui.name === 'foobar');
       child.props.resetUI();
       assert(child.props.ui.name === 'child');
-      assert(parent.props.ui.name === 'a');
+    });
+
+    it('resetting a parent UI resets its own AND child contexts', () => {
+        const tree = render(<UIParent><UIChild /></UIParent>);
+        const parent = TestUtils.findRenderedComponentWithType(tree, Parent);
+        const child = TestUtils.findRenderedComponentWithType(tree, Child);
+
+        child.props.updateUI({ name: 'b' });
+        parent.props.updateUI({ name: 'a' });
+        // Ensure state is updated
+        assert(child.props.ui.name === 'b');
+        // Resetting a parent should also reset all children
+        parent.props.resetUI();
+        assert(child.props.ui.name === 'child');
+        assert(parent.props.ui.name === 'parent');
+    });
+
+    it('resetting a child UI resets only its own context', () => {
+        const tree = render(<UIParent><UIChild /></UIParent>);
+        const parent = TestUtils.findRenderedComponentWithType(tree, Parent);
+        const child = TestUtils.findRenderedComponentWithType(tree, Child);
+
+        child.props.updateUI({ name: 'b' });
+        parent.props.updateUI({ name: 'a' });
+        // Ensure state is updated
+        assert(child.props.ui.name === 'b');
+        // Child should only reset itself to child; parent should stay changed
+        child.props.resetUI();
+        assert(child.props.ui.name === 'child');
+        assert(parent.props.ui.name === 'a');
+    });
   });
 });
