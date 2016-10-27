4b0dcb7a65106dc9fcb12c716e7d567b700b79e3
fezhengjin <fezhengjin@gmail.com>
2016-10-27 13:48:15 +0800

    New state return by action handler must be an object

--- src/action-reducer.js
+++ src/action-reducer.js
@@ -39,17 +39,22 @@ export default function reducer(state = defaultState, action) {
     const handleAction = value.get('handleAction');
     if (handleAction && handleAction.hasOwnProperty(action.type)) {
       const transforms = handleAction[action.type](value.toObject(), action);
-      state = state.withMutations( s => {
-        Object.keys(transforms).forEach(k => {
-          if (!value.has(k)) {
-            throw new Error(
-              `Couldn't find variable ${k} within your component's UI state ` +
-              `context. Define ${k} before using it in the @ui decorator`
-            );
-          }
-          s.setIn([key, k], transforms[k]);
+      if (
+        typeof transforms === 'object' &&
+        transforms.toString() === '[object Object]'
+        ) {
+        state = state.withMutations( s => {
+          Object.keys(transforms).forEach(k => {
+            if (!value.has(k)) {
+              throw new Error(
+                `Couldn't find variable ${k} within your component's UI state ` +
+                `context. Define ${k} before using it in the @ui decorator`
+              );
+            }
+            s.setIn([key, k], transforms[k]);
+          });
         });
-      });
+      }
     }
   });
 
