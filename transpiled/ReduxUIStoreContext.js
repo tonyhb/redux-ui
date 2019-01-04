372339752de06a5e826b2eefdee618bfb7c1a7e3
Manish K Singh <manish@tryhousecall.com>
2019-01-04 13:20:44 -0800

    Adding missing transpiled class

---
+++ transpiled/ReduxUIStoreContext.js
@@ -0,0 +1,14 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.ReduxUIStoreContext = undefined;
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var ReduxUIStoreContext = exports.ReduxUIStoreContext = _react2.default.createContext('store');
